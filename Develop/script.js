d4a4abfdc439daf82e9bb82981a6bf3d32ef6071
Marc Moten <marcmotenii@gmail.com>
2021-05-22 20:11:47 -0500

    added length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var length = 8;
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(length, 8, 128);
   var passwordText = document.querySelector("#password");
-
+  var long = prompt("How long is your password?")
+  length = long;
+  if (length > 8, length < 128) {
+    length = long;
+  console.log(length);
   passwordText.value = password;
 
+}
+else {
+  window.alert("put between 8 and 128 characters")
+}
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+  retVal = "";
+for (var i = 0, n = charset.length; i < length; ++i) {
+  retVal += charset.charAt(Math.floor(Math.random() * n));
+}
+return retVal;
+
+ 
+}
